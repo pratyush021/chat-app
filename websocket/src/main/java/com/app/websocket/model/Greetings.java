92906e292f69349433269d6b02fdf00b59fa816a
Pratyush Ranjan Bohidar <53327828+pratyush021@users.noreply.github.com>
2024-08-15 22:51:39 +0530

    websocket working

---
+++ websocket/src/main/java/com/app/websocket/model/Greetings.java
@@ -0,0 +1,13 @@
+package com.app.websocket.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Greetings {
+    private String content;
+
+}
